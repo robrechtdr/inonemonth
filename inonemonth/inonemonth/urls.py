3a292539027aa8af78f668d2b30ccfc2a2e4737a
Robrecht De Rouck <Robrecht.De.Rouck@gmail.com>
2013-12-09 16:38:08 +0100

    Implement homepage based on bootstrap's Jumbotron example

--- inonemonth/inonemonth/urls.py
+++ inonemonth/inonemonth/urls.py
@@ -6,7 +6,7 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^$', TemplateView.as_view(template_name='base.html')),
+    url(r'^$', TemplateView.as_view(template_name='home.html')),
 
     # Examples:
     # url(r'^$', 'inonemonth.views.home', name='home'),

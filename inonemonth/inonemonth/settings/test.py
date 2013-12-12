fb6286117d46981dd1120b470c9e0041843b93d9
Robrecht De Rouck <Robrecht.De.Rouck@gmail.com>
2013-12-12 11:56:36 +0100

    Improve execution speed of tests

--- inonemonth/inonemonth/settings/test.py
+++ inonemonth/inonemonth/settings/test.py
@@ -1,9 +1,25 @@
 from .base import *
 
-########## TEST SETTINGS
+########## TEST DISCOVER SETTINGS
 TEST_RUNNER = 'discover_runner.DiscoverRunner'
 TEST_DISCOVER_TOP_LEVEL = SITE_ROOT
 TEST_DISCOVER_ROOT = SITE_ROOT
 TEST_DISCOVER_PATTERN = "test_*.py"
+########## END TEST DISCOVER SETTINGS
+
 ########## IN-MEMORY TEST DATABASE
-DATABASES["default"]["NAME"] = ":memory:"
+#DATABASES["default"]["NAME"] = ":memory:"
+
+########## DATABASE CONFIGURATION
+# Time diff for 1 testcase, with PostgreSQL 9.4 secs, SQLite 0.5 secs.
+# However, run tests in local(with PostgreSQL) at least before merging into
+# production to test for RDBMS specific results.
+# See: http://www.celerity.com/blog/2013/04/29/how-write-speedy-unit-tests-django-part-1-basics/
+#      https://docs.djangoproject.com/en/dev/ref/settings/#databases
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'test.db',
+    }
+}
+########## END DATABASE CONFIGURATION

9b9f63a315327998d27a48c2a11c09900876b855
Robrecht De Rouck <Robrecht.De.Rouck@gmail.com>
2013-12-12 12:45:26 +0100

    Force test database to be in-memory

--- inonemonth/inonemonth/settings/test.py
+++ inonemonth/inonemonth/settings/test.py
@@ -1,5 +1,6 @@
 from .base import *
 
+
 ########## TEST DISCOVER SETTINGS
 TEST_RUNNER = 'discover_runner.DiscoverRunner'
 TEST_DISCOVER_TOP_LEVEL = SITE_ROOT
@@ -7,19 +8,18 @@ TEST_DISCOVER_ROOT = SITE_ROOT
 TEST_DISCOVER_PATTERN = "test_*.py"
 ########## END TEST DISCOVER SETTINGS
 
-########## IN-MEMORY TEST DATABASE
-#DATABASES["default"]["NAME"] = ":memory:"
 
-########## DATABASE CONFIGURATION
+########## IN-MEMORY TEST DATABASE CONFIGURATION
 # Time diff for 1 testcase, with PostgreSQL 9.4 secs, SQLite 0.5 secs.
 # However, run tests in local(with PostgreSQL) at least before merging into
 # production to test for RDBMS specific results.
 # See: http://www.celerity.com/blog/2013/04/29/how-write-speedy-unit-tests-django-part-1-basics/
 #      https://docs.djangoproject.com/en/dev/ref/settings/#databases
+#      https://www.sqlite.org/inmemorydb.html
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'test.db',
+        'NAME': ':memory:',
     }
 }
-########## END DATABASE CONFIGURATION
+########## END IN-MEMORY TEST DATABASE CONFIGURATION

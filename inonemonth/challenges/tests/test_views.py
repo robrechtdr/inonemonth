32d4ea98d419079a181df04f3ba59e95e23ad835
Robrecht De Rouck <Robrecht.De.Rouck@gmail.com>
2013-12-27 11:16:51 +0100

    Implement challenges views and tests

--- inonemonth/challenges/tests/test_views.py
+++ inonemonth/challenges/tests/test_views.py
@@ -1,35 +1,71 @@
 import django.test
-import settings
 
-from django.core.urlresolvers import reverse, resolve
+from urlparse import urlparse
+from django.conf import settings
+from django.core.urlresolvers import reverse, reverse_lazy, resolve
 from django.contrib.auth import authenticate
+from userena.models import UserenaSignup
 
-from ..views import challenge_create_view
+from core.tests.setups import UserFactory, ProfilePseudoFactory, ChallengeFactory
+from profiles.models import Profile
+
+from ..views import challenge_create_view, ChallengeDetailView, challenge_detail_view
+from ..models import Challenge
+
 
 class ChallengeCreateViewTestCase(django.test.TestCase):
     """
     Challenge_create_view test case.
     """
-
     def setUp(self):
         pass
 
-    def test_get_logged_in(self):
+    def test_get_request_non_logged_in_user_redirects(self):
         """
-        Verify if a get request by a logged in user returns the challenge creation page.
+        Verify if a get request of a non logged in user redirects to page
+        with login.
         """
         url = reverse("challenge_create_view")
-        # I'm getting status code 302(redirection) because I'm not logged in as
-        # a user.
+        response = self.client.get(url, follow=True)
+        # Test if there is a redirect
+        self.assertTrue(response.redirect_chain,
+                        msg="view should redirect")
 
-        #user = User.objects.get(pk=0)
-        #self.client.login(username=user.username, password="password")
-        #resp = self.client.get(url)
-        #self.assertEqual(resp.status_code, 200)
+        # Test if redirects to home page (which has login)
+        full_url_redirect = response.redirect_chain[0][0]
+        relative_url_redirect = urlparse(full_url_redirect).path
+        self.assertEqual(relative_url_redirect, reverse("home"))
 
-    def test_get_not_logged_in(self):
-        """
-        Verify if a user who is not logged in gets redirected to the home page.
-        """
-        pass
+    def test_get_request_logged_in_user_does_not_redirect(self):
+        url = reverse("challenge_create_view")
+        inactive_user = UserFactory()
+        # Activate user (same as confirming account with link in activation
+        # email)
+        active_user = UserenaSignup.objects.activate_user(inactive_user.userena_signup.activation_key)
+        # It is necessary to pass username as well to login
+        self.client.login(username=active_user.username, password="my_password", email=active_user.email)
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+
+class ChallengeDetailViewTestCase(django.test.TestCase):
+    """
+    Challenge_detail_view test case.
+    """
+    def test_url_resolves_to_view(self):
+        found = resolve("/challenge/detail/1/")
+        #self.assertEqual(found.func, challenge_detail_view)
+        self.assertEqual(found.func.func_name, ChallengeDetailView.as_view().func_name)
+
+    def test_detail_view(self):
+        # Create challenge. Can't view a challenge that doesn't exist yet ##
+        inactive_user = UserFactory()
+        active_user = UserenaSignup.objects.activate_user(inactive_user.userena_signup.activation_key)
+        Challenge.objects.create(title="My challenge",
+                                 description="This is my challenge",
+                                 lincoln=active_user.profile)
+        ####################################################################
+        url = reverse("challenge_detail_view", kwargs={"pk": 1})
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
 
--- inonemonth/challenges/views.py
+++ inonemonth/challenges/views.py
@@ -1,25 +1,34 @@
 from django.http import HttpResponseRedirect
+from django.contrib.auth import get_user_model
+from django.contrib.auth.decorators import user_passes_test
 from django.core.urlresolvers import reverse_lazy
 from django.shortcuts import render
 from django.views.generic import DetailView
 
+from profiles.models import Profile
+
 from .forms import ChallengeCreateModelForm
 from .models import Challenge
+from .decorators import user_has_profile
 
+
+@user_has_profile
 def challenge_create_view(request):
     if request.method == "POST":
         form = ChallengeCreateModelForm(request.POST)
         if form.is_valid():
-            # pre processing
+            # Get Profile based on username
+            profile = Profile.objects.get(user=request.user)
 
+            # Save data to challenge
             model = form.instance.__class__
             cleaned_dic = form.cleaned_data
-            #cleaned_dic.pop(field_name)  # If frm field not def in model
-            inst = model.objects.create(**cleaned_dic)
+            inst = model.objects.create(lincoln=profile, **cleaned_dic)
+            # If Judge selection here done then also add judges to model here.
             inst.save()
 
-            # post processing
-            return HttpResponseRedirect(reverse_lazy("challenge_detail_view", kwargs={"pk": inst.pk}))
+            return HttpResponseRedirect(reverse_lazy("challenge_detail_view",
+                                        kwargs={"pk": inst.pk}))
     else:
         form = ChallengeCreateModelForm()
 
@@ -31,3 +40,17 @@ class ChallengeDetailView(DetailView):
     template_name = "challenge/challenge_detail.html"
     model = Challenge
     context_object_name = "model"
+
+
+class challenge_detail_view():
+    pass
+
+'''
+class detail_view(request, challenge_id_code):
+    model = Challenge
+    inst = model.objects.get()
+    #decode_to_id(challenge_id_code)
+
+    return render(request=request, template_name='challenge/challenge_detail.html',
+      dictionary={"model": })
+'''

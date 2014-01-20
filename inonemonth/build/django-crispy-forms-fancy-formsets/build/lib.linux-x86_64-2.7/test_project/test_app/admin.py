584d96286da24235a11a68c767a138b1dc7baff5
Robrecht De Rouck <Robrecht.De.Rouck@gmail.com>
2014-01-20 10:25:29 +0100

    Remove build folder added accidentally

--- inonemonth/build/django-crispy-forms-fancy-formsets/build/lib.linux-x86_64-2.7/test_project/test_app/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from test_project.test_app.models import Author
-
-class AuthorAdmin(admin.ModelAdmin):
-    pass
-admin.site.register(Author, AuthorAdmin)
--- inonemonth/build/django-crispy-forms-fancy-formsets/test_project/fancy_formsets/helper.py
+++
@@ -1,9 +0,0 @@
-from crispy_forms.helper import FormHelper
-
-
-class InlineFormHelper(FormHelper):
-    pass
-
-
-class InlineFormsetHelper(FormHelper):
-    template_name = 'fancy_formsets_bootstrap/formset.html'

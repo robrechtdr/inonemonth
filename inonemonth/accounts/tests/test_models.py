9e4937cbe1af7a6a61d481092c70378bdc91429c
Robrecht De Rouck <Robrecht.De.Rouck@gmail.com>
2014-02-05 20:08:35 +0100

    Remove accounts app

--- inonemonth/accounts/tests/test_models.py
+++
@@ -1,6 +0,0 @@
-import django.test
-
-# In core:
-#import setups as tests_setups
-# In non-core:
-#import core.tests.setups as tests_setups
--- inonemonth/accounts/tests/test_views.py
+++
@@ -1,5 +0,0 @@
-import django.test
-import settings
-
-from django.core.urlresolvers import reverse, resolve
-from django.contrib.auth import authenticate

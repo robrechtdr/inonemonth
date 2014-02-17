2335db444a5a64f8ee224f8ab6648486b10ad6d5
Robrecht De Rouck <Robrecht.De.Rouck@gmail.com>
2014-02-17 13:35:23 +0100

    Make send_invitation_mail_to_juror pass

---
+++ inonemonth/core/mail_utils.py
@@ -0,0 +1,30 @@
+from django.conf import settings
+from django.core.mail import send_mail, EmailMultiAlternatives
+from django.core.urlresolvers import reverse
+from django.template import Context
+from django.template.loader import get_template
+
+
+def send_invitation_mail_to_juror(juror, request):
+    subject = 'Invitation to be a juror of my challenge'
+    from_role = juror.challenge.get_clencher()
+    from_email = from_role.user.email
+    to_email = juror.user.email
+    # Change pattern name once /account/signin-juror/challenge/1/ is made
+    link = request.build_absolute_uri(reverse("challenge_detail_view", kwargs={"pk": 1}))
+
+    text = get_template('email/juror_invitation.txt')
+    html = get_template('email/juror_invitation.html')
+    email_context = Context({
+        'sender': from_email,
+        'challenge_title': from_role.challenge.title,
+        'link': link,
+        'account_email': to_email,
+        'account_password': juror.user.userextension.temp_password
+    })
+    text_content = text.render(email_context)
+    html_content = html.render(email_context)
+
+    mail = EmailMultiAlternatives(subject, text_content, from_email, [to_email])
+    mail.attach_alternative(html_content, "text/html")
+    mail.send()
